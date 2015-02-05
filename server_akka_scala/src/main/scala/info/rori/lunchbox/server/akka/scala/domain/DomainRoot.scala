f14ea33efdcb074125d119e0f1d9b2ab0ee9ad25
rori <mail@rori.info>
2015-02-05 19:11:43 +0100

    LunchProviderService wird beim Boot gestartet

--- server_akka_scala/src/main/scala/info/rori/lunchbox/server/akka/scala/domain/DomainRoot.scala
+++ server_akka_scala/src/main/scala/info/rori/lunchbox/server/akka/scala/domain/DomainRoot.scala
@@ -1,6 +1,7 @@
 package info.rori.lunchbox.server.akka.scala.domain
 
 import akka.actor._
+import info.rori.lunchbox.server.akka.scala.domain.service.LunchProviderService
 
 /**
  * Erstellt und überwacht die Domain-Services.
@@ -14,6 +15,8 @@ object DomainRoot {
 class DomainRoot
   extends Actor
   with ActorLogging {
+
+  val lunchProviderService = context.actorOf(LunchProviderService.props, LunchProviderService.Name)
 
   override def receive: Receive = {
     case msg => log.warning("unhandled message in DomainRoot: " + msg)
