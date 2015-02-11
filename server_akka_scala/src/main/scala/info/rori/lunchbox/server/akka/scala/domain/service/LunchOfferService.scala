b493d58ee681d2f7854a87e406d89133671d5536
rori <mail@rori.info>
2015-02-11 19:24:25 +0100

    Domain: Bezeichnungen verbessert

--- server_akka_scala/src/main/scala/info/rori/lunchbox/server/akka/scala/domain/service/LunchOfferService.scala
+++ server_akka_scala/src/main/scala/info/rori/lunchbox/server/akka/scala/domain/service/LunchOfferService.scala
@@ -11,18 +11,18 @@ object LunchOfferService {
   case object GetAll
   case class GetById(id: Id)
   case class GetByDay(day: LocalDate)
-  case class MultiResult(providers: Seq[LunchOffer])
-  case class SingleResult(provider: Option[LunchOffer])
+  case class MultiResult(offers: Seq[LunchOffer])
+  case class SingleResult(offer: Option[LunchOffer])
 }
 
 class LunchOfferService extends Actor {
-  private val providers = Seq[LunchOffer]()
+  private val offers = Seq[LunchOffer]()
 
   import LunchOfferService._
 
   override def receive = {
-    case GetAll => sender ! MultiResult(providers)
-    case GetById(id) => sender ! SingleResult(providers.find(_.id == id))
-    case GetByDay(day) => sender ! MultiResult(providers.filter(_.day == day))
+    case GetAll => sender ! MultiResult(offers)
+    case GetById(id) => sender ! SingleResult(offers.find(_.id == id))
+    case GetByDay(day) => sender ! MultiResult(offers.filter(_.day == day))
   }
 }
