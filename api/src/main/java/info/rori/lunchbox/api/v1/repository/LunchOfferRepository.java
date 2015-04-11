9c0d285296cb5fc5c9d71fc190014fc9d129a98d
rori <mail@rori.info>
2015-04-11 15:25:19 +0200

    - variable Dependency-Revisionen sind schlecht für Zugfahrten - (immer) aktuelle Mock-Daten sind vorteilhaft

--- api/src/main/java/info/rori/lunchbox/api/v1/repository/LunchOfferRepository.java
+++ api/src/main/java/info/rori/lunchbox/api/v1/repository/LunchOfferRepository.java
@@ -3,7 +3,6 @@ package info.rori.lunchbox.api.v1.repository;
 import info.rori.lunchbox.api.v1.model.LunchOffer;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,14 +15,14 @@ public class LunchOfferRepository {
     private static List<LunchOffer> list = new ArrayList<>();
 
     static {
-        list.add(new LunchOffer(1, "Hähnchenbrustfilet auf Ratatouille mit Gnocchi", LocalDate.of(2015, Month.JANUARY, 14), 580, 1));
-        list.add(new LunchOffer(2, "Schweinegeschnetzeltes Gyros Art mit Tsatsiki und Pommes frites", LocalDate.of(2015, Month.JANUARY, 13), 580, 1));
+        list.add(new LunchOffer(1, "Hähnchenbrustfilet auf Ratatouille mit Gnocchi", LocalDate.now().plusDays(1), 580, 1));
+        list.add(new LunchOffer(2, "Schweinegeschnetzeltes Gyros Art mit Tsatsiki und Pommes frites", LocalDate.now(), 580, 1));
 
-        list.add(new LunchOffer(3, "Spaghetti Napoli mit schwarzen Oliven, Kapern & geriebenem Käse", LocalDate.of(2015, Month.JANUARY, 13), 520, 2));
-        list.add(new LunchOffer(4, "Schweinebraten an Rahmsauce mit glacierten Karotten und Petersilienkartoffeln", LocalDate.of(2015, Month.JANUARY, 13), 550, 2));
+        list.add(new LunchOffer(3, "Spaghetti Napoli mit schwarzen Oliven, Kapern & geriebenem Käse", LocalDate.now(), 520, 2));
+        list.add(new LunchOffer(4, "Schweinebraten an Rahmsauce mit glacierten Karotten und Petersilienkartoffeln", LocalDate.now(), 550, 2));
 
-        list.add(new LunchOffer(5, "Grüne Bohneneintopf mit Rindfleisch", LocalDate.of(2015, Month.JANUARY, 13), 370, 5));
-        list.add(new LunchOffer(6, "Hausgemachtes Hacksteak „Toskana“ mit Feta & Tomaten, Krautsalat und Pommes frites", LocalDate.of(2015, Month.JANUARY, 13), 500, 5));
+        list.add(new LunchOffer(5, "Grüne Bohneneintopf mit Rindfleisch", LocalDate.now(), 370, 5));
+        list.add(new LunchOffer(6, "Hausgemachtes Hacksteak „Toskana“ mit Feta & Tomaten, Krautsalat und Pommes frites", LocalDate.now(), 500, 5));
     }
 
     public List<LunchOffer> findAll() {
