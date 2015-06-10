054580666cde638ec628b50c6d0e190febde5e97
rori <mail@rori.info>
2015-06-10 10:20:50 +0200

    WebApp: MainService-Kommentar verbessert

--- client_web_angular/src/app/main/main.service.js
+++ client_web_angular/src/app/main/main.service.js
@@ -3,14 +3,14 @@
 // App-Modul abrufen
 var app = angular.module('lunchboxWebapp');
 
-// LunchProvider-Service bereitstellen
+// Service zum Abrufen der LunchProvider bereitstellen
 app.service('LunchProviderStore', function($resource) {
     return $resource('api/v1/lunchProvider/:providerId', {providerId:'@id'}, {
       query: {method:'GET', isArray: true} // nur das GET über alle Werte wird benötigt
   });
 });
 
-// Factory zum Erzeugen des LunchOffer-Service bereitstellen
+// Service zum Abrufen der LunchOffer bereitstellen
 app.service('LunchOfferStore', function($resource) {
     return $resource('api/v1/lunchOffer/:offerId', {offerId:'@id'}, {
       query: {method:'GET', isArray: true} // nur das GET über alle Werte wird benötigt
