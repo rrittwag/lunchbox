05f942b9cc9b6fd3585374ad53552f4fd8f2059e
rori <mail@rori.info>
2016-05-21 00:51:22 +0200

    Server: Vereinfachung des Content-Type im Feed

--- server_play_akka_scala/app/util/feed/AtomModel.scala
+++ server_play_akka_scala/app/util/feed/AtomModel.scala
@@ -3,6 +3,7 @@ package util.feed
 import java.net._
 
 import org.joda.time.DateTime
+import play.api.http.MimeTypes
 
 
 case class AtomFeed(id: URI,
@@ -25,7 +26,14 @@ case class Content(contentType: String,
                   )
 
 object Content {
-  def apply(content: play.twirl.api.Content): Content = apply(content.contentType, content.body)
+  def apply(content: play.twirl.api.Content): Content = {
+    val contentType = content.contentType match {
+      case MimeTypes.HTML => "html"
+      case MimeTypes.TEXT => "text"
+      case mime => mime
+    }
+    apply(contentType, content.body)
+  }
 }
 
 case class Author(name: String)
