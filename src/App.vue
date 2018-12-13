645488b4c33fd8ab0c82c10ffdab3fdb05ea42a2
rori <mail@rori.info>
2018-12-13 22:12:37 +0100

    refactor(module import/export): Vue-Komponenten benötigen einen Default-Export, daher sind Default-Importe konsistenter als index.ts-Importe

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 <script lang="ts">
 import { Component, Vue, Provide } from 'vue-property-decorator'
 import { getModule } from 'vuex-module-decorators'
-import { Layout } from '@/views/layout'
 import { LunchStore } from '@/store'
+import Layout from '@/views/layout/Layout.vue'
 
 // vscode shows decorator errors -> https://github.com/vuejs/vetur/issues/815
 @Component({
