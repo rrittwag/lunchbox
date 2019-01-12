53133d6230c6c342ab1ef6de5a8800d3da065063
rori <mail@rori.info>
2019-01-12 17:48:06 +0100

    style: indendation

--- src/views/offers/OfferBoxGroup.vue
+++ src/views/offers/OfferBoxGroup.vue
@@ -1,17 +1,17 @@
 <template>
   <b-container fluid class='offer-box-group'>
-      <b-row>
-        <b-col
-          sm='6' md='4' lg='3'
-          v-for='provider in visibleProviders'
-          :key='provider.id'
-        >
-          <OfferBox
-            :provider='provider'
-            :offers='visibleOffersOf(provider)'
-          />
-        </b-col>
-      </b-row>
+    <b-row>
+      <b-col
+        sm='6' md='4' lg='3'
+        v-for='provider in visibleProviders'
+        :key='provider.id'
+      >
+        <OfferBox
+          :provider='provider'
+          :offers='visibleOffersOf(provider)'
+        />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
