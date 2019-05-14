97935406a8b2bcc6794fc5f1f19761d00ffbf724
rori <mail@rori.info>
2019-05-14 19:46:27 +0200

    chore: Provider-URLs ergänzt

--- src/main/kotlin/lunchbox/domain/models/LunchProvider.kt
+++ src/main/kotlin/lunchbox/domain/models/LunchProvider.kt
@@ -17,12 +17,12 @@ enum class LunchProvider(
   val active: Boolean = true
 ) {
   SCHWEINESTALL(1, "Schweinestall", NEUBRANDENBURG, URL("http://www.schweinestall-nb.de/index.php?id=159")),
-  HOTEL_AM_RING(2, "Hotel am Ring", NEUBRANDENBURG, URL("http://example.com"), false),
-  AOK_CAFETERIA(3, "AOK Cafeteria", NEUBRANDENBURG, URL("http://example.com")),
-  SUPPENKULTTOUR(4, "Suppenkulttour", NEUBRANDENBURG, URL("http://example.com")),
-  SALT_N_PEPPER(5, "Salt 'n' Pepper", BERLIN_SPRINGPFUHL, URL("http://example.com")),
-  GESUNDHEITSZENTRUM(6, "Gesundheitszentrum", BERLIN_SPRINGPFUHL, URL("http://example.com")),
-  FELDKUECHE(7, "Feldküche Karow", BERLIN_SPRINGPFUHL, URL("http://example.com")),
-  DAS_KRAUTHOF(8, "Das Krauthof", NEUBRANDENBURG, URL("http://example.com")),
-  TABBOULEH(9, "Tabbouleh", BERLIN_SPRINGPFUHL, URL("http://example.com"))
+  HOTEL_AM_RING(2, "Hotel am Ring", NEUBRANDENBURG, URL("http://www.hotel-am-ring.de/restaurant-rethra.html"), false),
+  AOK_CAFETERIA(3, "AOK Cafeteria", NEUBRANDENBURG, URL("https://www.hotel-am-ring.de/aok-cafeteria.html")),
+  SUPPENKULTTOUR(4, "Suppenkulttour", NEUBRANDENBURG, URL("https://www.suppenkult.com/wochenplan.html")),
+  SALT_N_PEPPER(5, "Salt 'n' Pepper", BERLIN_SPRINGPFUHL, URL("https://www.partyservice-rohde.de/bistro-angebot-der-woche")),
+  GESUNDHEITSZENTRUM(6, "Gesundheitszentrum", BERLIN_SPRINGPFUHL, URL("https://de-de.facebook.com/pages/Kantine-BQuakatz-Allee-der-Kosmonauten/181190361991823")),
+  FELDKUECHE(7, "Feldküche Karow", BERLIN_SPRINGPFUHL, URL("https://www.feldkuechebkarow.de/speiseplan")),
+  DAS_KRAUTHOF(8, "Das Krauthof", NEUBRANDENBURG, URL("https://www.daskrauthof.de/karte")),
+  TABBOULEH(9, "Tabbouleh", BERLIN_SPRINGPFUHL, URL("https://www.restaurant-tabbouleh.de/menu"))
 }
