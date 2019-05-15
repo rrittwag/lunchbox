44f0bf5b7f9edb8a0f7d5e7b068c35395248d011
rori <mail@rori.info>
2019-05-15 20:00:58 +0200

    chore: improve readabilty

--- src/test/kotlin/lunchbox/util/json/MoneySerializerTest.kt
+++ src/test/kotlin/lunchbox/util/json/MoneySerializerTest.kt
@@ -5,6 +5,7 @@ import org.joda.money.Money
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.skyscreamer.jsonassert.JSONAssert.assertEquals
+import org.skyscreamer.jsonassert.JSONCompareMode
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.json.JsonTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -19,8 +20,10 @@ class MoneySerializerTest {
   @Test
   fun serialize() {
     val entity = TestEntityWithMoney(Money.parse("EUR 15.20"))
+
     val json = mapper.writeValueAsString(entity)
-    assertEquals(json, "{ price: 1520 }", true)
+
+    assertEquals(json, "{ price: 1520 }", JSONCompareMode.STRICT)
   }
 }
 
