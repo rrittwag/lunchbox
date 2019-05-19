b0964a36f3a4621091a7943faa0c0ef8c82b6c2e
rori <mail@rori.info>
2019-05-19 16:11:00 +0200

    refactor(): Best Practices angewendet

--- src/test/kotlin/lunchbox/util/json/MoneySerializerTest.kt
+++ src/test/kotlin/lunchbox/util/json/MoneySerializerTest.kt
@@ -3,15 +3,12 @@ package lunchbox.util.json
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.joda.money.Money
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.skyscreamer.jsonassert.JSONAssert.assertEquals
 import org.skyscreamer.jsonassert.JSONCompareMode
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.json.JsonTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @JsonTest
-@ExtendWith(SpringExtension::class)
 class MoneySerializerTest(
   @Autowired val mapper: ObjectMapper
 ) {
