5622f33be61b81316e2470e5ec5c21f35db74b7b
rori <mail@rori.info>
2019-09-06 21:14:30 +0200

    chore: Formatierung

--- src/main/kotlin/lunchbox/config/ConfigLunchResolver.kt
+++ src/main/kotlin/lunchbox/config/ConfigLunchResolver.kt
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class ConfigLunchResolver {
   @Bean
-  fun dateValidator(): DateValidator =
+  fun dateValidator() =
     DateValidator.validFromMondayLastWeek()
 }
