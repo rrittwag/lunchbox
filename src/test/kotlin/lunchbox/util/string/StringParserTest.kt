7a2f15302b1c5314ab46a422d63da37ed9c25218
rori <mail@rori.info>
2019-10-01 20:13:02 +0200

    chore: Unit-Test ergänzt

---
+++ src/test/kotlin/lunchbox/util/string/StringParserTest.kt
@@ -0,0 +1,21 @@
+package lunchbox.util.string
+
+import lunchbox.domain.resolvers.weekOf
+import org.amshove.kluent.shouldEqual
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class StringParserTest {
+
+  @Test
+  fun `parseMondayOfMostUsedWeek by PDF url`() {
+    fun parse(lines: List<String>): LocalDate? = StringParser.parseMondayOfMostUsedWeek(lines)
+
+    val yearNow = LocalDate.now().year
+
+    parse(listOf("16.03.-20.03.2016")) shouldEqual weekOf("2016-03-20").monday
+    parse(listOf("23.03.-27.03.")) shouldEqual weekOf("$yearNow-03-27").monday
+    parse(listOf("16.03.-20.03.")) shouldEqual weekOf("$yearNow-03-20").monday
+    parse(listOf("27.07.-31.07.2015 bla")) shouldEqual weekOf("2015-07-27").monday
+  }
+}
