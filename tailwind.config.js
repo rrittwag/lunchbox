28f2dae68deb4b2d80f8ccb7ca1fdb47133b37e5
rrittwag <mail@rori.info>
2019-11-15 18:29:30 +0100

    chore: fix lint errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        primary: "var(--color-bg-primary)",
-        secondary: "var(--color-bg-secondary)"
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)'
       },
       textColor: {
-        primary: "var(--color-text-primary)",
-        accent: "var(--color-text-accent)"
+        primary: 'var(--color-text-primary)',
+        accent: 'var(--color-text-accent)'
       },
     }
   },
