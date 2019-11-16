2f8b1bc5479ed88c4c2296ad82e514e8dd7326de
rrittwag <mail@rori.info>
2019-11-16 07:38:53 +0100

    chore: unnötigen Kommentar entfernt

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -9,7 +9,6 @@
 import { Component, Vue } from 'vue-property-decorator'
 import { Header, Content } from './layout/'
 
-// vscode shows decorator errors -> https://github.com/vuejs/vetur/issues/815
 @Component({
   components: {
     Header,
