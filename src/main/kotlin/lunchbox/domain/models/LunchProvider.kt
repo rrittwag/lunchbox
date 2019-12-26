ffa984dfbda7dce681892c88e58e74ba2292fe15
rori <mail@rori.info>
2019-12-26 11:21:18 +0100

    chore: Die Kantine im Gesundheitszentrum ist dauerhaft geschlossen.

--- src/main/kotlin/lunchbox/domain/models/LunchProvider.kt
+++ src/main/kotlin/lunchbox/domain/models/LunchProvider.kt
@@ -23,7 +23,7 @@ enum class LunchProvider(
   AOK_CAFETERIA(3, "AOK Cafeteria", NEUBRANDENBURG, URL("https://www.hotel-am-ring.de/aok-cafeteria.html")),
   SUPPENKULTTOUR(4, "Suppenkulttour", NEUBRANDENBURG, URL("https://www.suppenkult.com/wochenplan.html")),
   SALT_N_PEPPER(5, "Salt 'n' Pepper", BERLIN_SPRINGPFUHL, URL("https://www.partyservice-rohde.de/bistro-angebot-der-woche")),
-  GESUNDHEITSZENTRUM(6, "Gesundheitszentrum", BERLIN_SPRINGPFUHL, URL("https://de-de.facebook.com/pages/Kantine-BQuakatz-Allee-der-Kosmonauten/181190361991823")),
+  GESUNDHEITSZENTRUM(6, "Gesundheitszentrum", BERLIN_SPRINGPFUHL, URL("https://de-de.facebook.com/pages/Kantine-BQuakatz-Allee-der-Kosmonauten/181190361991823"), false),
   FELDKUECHE(7, "Feldküche Karow", BERLIN_SPRINGPFUHL, URL("https://www.feldkuechebkarow.de/speiseplan")),
   DAS_KRAUTHOF(8, "Das Krauthof", NEUBRANDENBURG, URL("https://www.daskrauthof.de/karte")),
   TABBOULEH(9, "Tabbouleh", BERLIN_SPRINGPFUHL, URL("https://www.restaurant-tabbouleh.de/menu"), false)
