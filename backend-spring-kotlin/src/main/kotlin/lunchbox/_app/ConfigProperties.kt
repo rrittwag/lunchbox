fceb0ac0de8ab8781bf0957c6d6c8ad04243f8ef
rori <mail@rori.info>
2020-12-18 11:48:17 +0100

    chore(backend): Beispiel für ConfigurationProperties

---
+++ backend-spring-kotlin/src/main/kotlin/lunchbox/_app/ConfigProperties.kt
@@ -0,0 +1,8 @@
+package lunchbox._app
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan("lunchbox")
+class ConfigProperties
--- backend-spring-kotlin/src/main/kotlin/lunchbox/util/facebook/FacebookGraphApiImpl.kt
+++ backend-spring-kotlin/src/main/kotlin/lunchbox/util/facebook/FacebookGraphApiImpl.kt
@@ -1,23 +1,30 @@
 package lunchbox.util.facebook
 
 import java.time.Duration
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.util.retry.Retry.backoff
+
+@ConstructorBinding
+@ConfigurationProperties("external.facebook")
+data class FacebookConfigProperties(
+  val appId: String = "",
+  val appSecret: String = ""
+)
 
 /**
  * Ruft eine Resource der Facebook GraphApi ab.
  */
 @Component
 class FacebookGraphApiImpl(
-  @Value("\${external.facebook.appId:}") val appId: String,
-  @Value("\${external.facebook.appSecret:}") val appSecret: String
+  val config: FacebookConfigProperties
 ) : FacebookGraphApi {
 
   override fun <T : GraphApiResource> query(url: String, clazz: Class<T>): T? {
     val resource = url.replaceFirst(Regex("^/"), "")
-    val accessToken = "access_token=$appId|$appSecret"
+    val accessToken = "access_token=${config.appId}|{${config.appSecret}}"
 
     return WebClient.create("https://graph.facebook.com/v2.10/$resource?$accessToken")
       .get()
