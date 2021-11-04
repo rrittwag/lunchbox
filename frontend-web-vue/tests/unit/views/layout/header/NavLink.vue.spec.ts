9626db3c9e1ce95f27495be409470b0c2ec64aab
rori <mail@rori.info>
2021-11-04 06:02:52 +0100

    chore(pipeline): Snapshot-Tests von Default-Locale en_US auf de_DE zwingen

--- frontend-web-vue/tests/unit/views/layout/header/NavLink.vue.spec.ts
+++ frontend-web-vue/tests/unit/views/layout/header/NavLink.vue.spec.ts
@@ -1,7 +1,6 @@
 import NavLink from '@/views/layout/header/NavLink.vue'
 import { mount } from '@vue/test-utils'
 import { createMemoryHistory, createRouter, RouteRecordRaw } from 'vue-router'
-import { defineComponent } from 'vue'
 
 describe('NavLink', () => {
   test('renders snapshot', () => {
