9e25e3ffe15c31dcec855269239849100cdd8503
rori <mail@rori.info>
2022-02-24 07:44:48 +0100

    chore: simplified PWA config

--- frontend-web-vue/vite.config.js
+++ frontend-web-vue/vite.config.js
@@ -48,26 +48,6 @@ export default defineConfig({
       workbox: {
         runtimeCaching: [
           {
-            // cache fonts
-            urlPattern: '/.(?:woff2?)$/',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'fonts',
-              expiration: { maxAgeSeconds: 30 * 24 * 60 * 60, maxEntries: 5 },
-              cacheableResponse: { statuses: [0, 200] },
-            },
-          },
-          {
-            // cache images
-            urlPattern: '/.(?:png|gif|jpg|jpeg|webp|svg)$/',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images',
-              expiration: { maxAgeSeconds: 30 * 24 * 60 * 60, maxEntries: 60 },
-              cacheableResponse: { statuses: [0, 200] },
-            },
-          },
-          {
             // cache API calls
             urlPattern: /api/,
             handler: 'NetworkFirst',
