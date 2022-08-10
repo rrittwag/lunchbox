e6be2cbab405b46e5f2402e2d88070d99dab6964
rori <mail@rori.info>
2022-08-10 19:38:17 +0200

    chore(frontend): upgrade dependencies

--- frontend-web-vue/.eslintrc.js
+++ frontend-web-vue/.eslintrc.js
@@ -1,14 +1,13 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  env: {
-    node: true,
-    'vue/setup-compiler-macros': true,
-  },
   extends: [
-    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
   rules: {
     'no-console': 'error',
--- frontend-web-vue/tailwind.config.js
+++ frontend-web-vue/tailwind.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.vue', './src/**/*.ts'],
   theme: {
