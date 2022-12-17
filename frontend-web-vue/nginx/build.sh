b9da6cc88c18bb13c5ac5b6c6c61e839fa9f80f9
rori <mail@rori.info>
2022-12-17 16:57:39 +0100

    chore(frontend): smaller Docker image

--- frontend-web-vue/nginx/build.sh
+++ frontend-web-vue/nginx/build.sh
@@ -3,4 +3,5 @@
 # generate Docker image for latest nginx with brotli compression
 # -> https://github.com/nginxinc/docker-nginx/tree/master/modules
 wget -O Dockerfile.nginx https://raw.githubusercontent.com/nginxinc/docker-nginx/master/modules/Dockerfile.alpine
+sed -i 's/mainline-alpine/mainline-alpine-slim/g' Dockerfile.nginx
 docker build -f Dockerfile.nginx --build-arg ENABLED_MODULES="brotli" -t docker.rori.dev/nginx-brotli:latest --pull .
