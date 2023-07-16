47b00b402e7250e7806d6e067060bf67c31f68cc
rori <mail@rori.info>
2023-07-16 22:54:43 +0200

    chore: fixing nginx usage

--- frontend-web-vue/nginx/build.sh
+++ frontend-web-vue/nginx/build.sh
@@ -3,5 +3,10 @@
 # generate Docker image for latest nginx with brotli compression
 # -> https://github.com/nginxinc/docker-nginx/tree/master/modules
 wget -O Dockerfile.nginx https://raw.githubusercontent.com/nginxinc/docker-nginx/master/modules/Dockerfile.alpine
-sed -i 's/mainline-alpine/mainline-alpine-slim/g' Dockerfile.nginx
-docker build -f Dockerfile.nginx --build-arg ENABLED_MODULES="brotli" -t docker.rori.dev/nginx-brotli:latest --pull .
+
+docker build \
+  --file Dockerfile.nginx \
+  --build-arg NGINX_FROM_IMAGE=nginx:mainline-alpine-slim \
+  --build-arg ENABLED_MODULES="brotli" \
+  --tag docker.rori.dev/nginx-brotli:latest \
+  --pull .
