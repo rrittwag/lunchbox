67caae6d242b5d9c91b8f19ad15a45d3e2db537a
rori <mail@rori.info>
2023-08-25 07:03:56 +0200

    chore: update @testing-library/jest-dom to v6

--- frontend-web-vue/tests/setup.ts
+++ frontend-web-vue/tests/setup.ts
@@ -1,9 +1,8 @@
-import { expect, afterEach } from 'vitest'
+import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/vue'
-import matchers from '@testing-library/jest-dom/matchers'
 
 // extends Vitest's expect method with methods from jest-dom
-expect.extend(matchers)
+import '@testing-library/jest-dom/vitest'
 
 // Testing Library uses CSS to determine visible elements
 import '@/assets/style/index.scss'
