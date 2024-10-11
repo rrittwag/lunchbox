404b8abf751838264b96f5292b91a34d190f4869
rori <mail@rori.info>
2024-10-11 19:08:38 +0200

    chore: eslint config ist nun vollständig FlatConfig

--- frontend-web-vue/eslint.config.js
+++ frontend-web-vue/eslint.config.js
@@ -1,28 +1,13 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-})
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import prettierConfig from '@vue/eslint-config-prettier'
 
 export default [
   js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
-  ...compat.extends('@vue/eslint-config-typescript/recommended'),
-  ...compat.extends('@vue/eslint-config-prettier'),
-  {
-    files: ['**/*.vue', '**/*.js', '**/*.ts'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-    },
-  },
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  prettierConfig,
   {
     rules: {
       'no-console': 'error',
